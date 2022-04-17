e91b670e802d2ed45fa3a10b04c296934ac566cf
Shifali Gwal <gwal.shifali2223@gmail.com>
2022-04-17 13:07:51 +0530

    Fetch notes from backend

--- src/pages/MyNotes/MyNotes.js
+++ src/pages/MyNotes/MyNotes.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import { Badge, Button, Card } from "react-bootstrap";
-// import Accordion from "react-bootstrap/Accordion";
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
 import { MainScreen } from "../../components/MainScreen/MainScreen";
-import notes from "../../DummyData/notes";
+// import notes from "../../DummyData/notes";
 
-const deleteHandler = (id) => {
-  if (window.confirm("Are you sure?")) {
-  }
-};
+
 
 const MyNotes = () => {
+
+  const [notes, setNotes] = useState([])
+
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+    }
+  };
+
+  const fetchNotes = async () => {
+    const data = await axios.get("/api/notes");
+
+    setNotes(data);
+  };
+
   console.log(notes);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  // console.log(notes);
   return (
     <MainScreen title="Welcome back Shifali Gwal">
       <Link to="createnote">
@@ -19,42 +36,48 @@ const MyNotes = () => {
           Create New Note
         </Button>
       </Link>
-      {notes.map((note) => (
-        <Card key={notes.id} style={{ margin: 10 }}>
-          <Card.Header style={{ display: "flex" }}>
-            <span
-              style={{
-                fontSize: 25,
-                cursor: "pointer",
-                alignSelf: "center",
-                flex: 1,
-              }}
-            >
-              {note.title}
-            </span>
-            <div>
-              <Button href={`/notes/${note._id}`}>Edit</Button>
-              <Button
-                variant="danger"
-                className="mx-2"
-                onClick={() => deleteHandler(note._id)}
-              >
-                Delete
-              </Button>
-            </div>
-          </Card.Header>
-
-          <Card.Body>
-            <h4>
-              <Badge variant="success">Category - {note.category}</Badge>
-            </h4>
-            <blockquote className="blockquote mb-0">
-              <p>{note.content}</p>
-              <footer className="blockquote-footer">Created on - Date</footer>
-            </blockquote>
-          </Card.Body>
-        </Card>
-      ))}
+      {!!notes?.data &&
+        notes.data.map((note) => (
+          <Accordion key={note._id}>
+            <Card style={{ margin: 10 }}>
+              <Card.Header style={{ display: "flex" }}>
+                <span
+                  style={{
+                    fontSize: 25,
+                    cursor: "pointer",
+                    alignSelf: "center",
+                    flex: 1,
+                  }}
+                >
+                  <Accordion.Header>{note.title}</Accordion.Header>
+                </span>
+                <div>
+                  <Button href={`/notes/${note._id}`}>Edit</Button>
+                  <Button
+                    variant="danger"
+                    className="mx-2"
+                    onClick={() => deleteHandler(note._id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </Card.Header>
+              <Accordion.Body>
+                <Card.Body>
+                  <h4>
+                    <Badge variant="success">Category - {note.category}</Badge>
+                  </h4>
+                  <blockquote className="blockquote mb-0">
+                    <p>{note.content}</p>
+                    <footer className="blockquote-footer">
+                      Created on - Date
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Body>
+            </Card>
+          </Accordion>
+        ))}
     </MainScreen>
   );
 };
